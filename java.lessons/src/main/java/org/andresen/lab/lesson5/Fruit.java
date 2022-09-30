1c861950ac7aa473703cc216e77220ce8819207b
Maria Muravyeva <marie.muraveva@gmail.com>
2022-09-30 23:02:50 +0600

    Lesson 5

---
+++ java.lessons/src/main/java/org/andresen/lab/lesson5/Fruit.java
@@ -0,0 +1,13 @@
+package org.andresen.lab.lesson5;
+
+public class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
